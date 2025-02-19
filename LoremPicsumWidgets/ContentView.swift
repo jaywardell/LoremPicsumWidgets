e2ea87ea20c6f9b84d08a2e566e6c981044d59de
Joseph Wardell <jay@jaywardell.me>
2025-02-18 22:50:38 -0500

    load a list of images and use them

--- LoremPicsumWidgets/ContentView.swift
+++ LoremPicsumWidgets/ContentView.swift
@@ -10,10 +10,10 @@ import SwiftUI
 struct ContentView: View {
     
     @State private var image: UIImage?
+    @State private var imageIDs: [Int] = []
     
     // the largest possible size of a large widget is 379 x 379 (on 12.9 inch iPad)
-    // according to
-    // https://github.com/simonbs/ios-widget-sizeshttps://github.com/simonbs/ios-widget-sizes,
+    // according to https://github.com/simonbs/ios-widget-sizes
     // TODO: if on iPhone, only fetch the largest possible value for an iPhone's large widget size to save network traffic
     // TODO: if on iPad, fetch the largest possible value for an iPad's extra large widget size
     // TODO: (harder) fetch the largest possible widget size for the current model device using
@@ -43,9 +43,36 @@ struct ContentView: View {
     }
     
     private func loadRandomImage() {
-        let pictureURL = LoremPicsum.randomPicture(width: Int(pictureSize.width), height: Int(pictureSize.height)).jpg().url
         
+        // as a shortcut, just grab imageIDs here
         Task.detached {
+            
+            // load a list of possible image ids from the server
+            if await self.imageIDs.isEmpty {
+                let imageIDsURL = LoremPicsum.list(page: 1, picturesPerPage: 100)
+                do {
+                    let (data, _) = try await URLSession.shared.data(from: imageIDsURL)
+                    struct ResponseObject: Decodable { let id: String }
+                    let ids = try JSONDecoder().decode([ResponseObject].self, from: data)
+                    await MainActor.run {
+                        self.imageIDs = ids.map(\.id).compactMap(Int.init)
+                    }
+                }
+                catch {
+                    print("Error retrieving list of image ids from \(imageIDsURL): \(error.localizedDescription)")
+                    return
+                }
+            }
+            
+            
+            // choose a random image id and request it from the server
+            guard let imageID = await self.imageIDs.randomElement() else { return }
+            let pictureURL = LoremPicsum.picture(
+                id: imageID,
+                width: Int(pictureSize.width),
+                height: Int(pictureSize.height)
+            ).url
+            
             do {
                 let (data, _) = try await URLSession.shared.data(from: pictureURL)
                 await MainActor.run {
