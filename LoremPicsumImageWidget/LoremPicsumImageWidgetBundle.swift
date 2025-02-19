24b60ce7587650cbce2a3bda9994c40c2a02f7a4
Joseph Wardell <jay@jaywardell.me>
2025-02-18 23:25:57 -0500

    removed old widget target and added a new one

---
+++ LoremPicsumImageWidget/LoremPicsumImageWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  LoremPicsumImageWidgetBundle.swift
+//  LoremPicsumImageWidget
+//
+//  Created by Joseph Wardell on 2/18/25.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct LoremPicsumImageWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        LoremPicsumImageWidget()
+    }
+}
